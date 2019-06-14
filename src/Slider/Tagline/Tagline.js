c75ed3f364030abb7c491897c62941df030b3de4
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-06-14 14:42:37 +0200

    fix: typo bug

--- src/Slider/Tagline/Tagline.js
+++ src/Slider/Tagline/Tagline.js
@@ -3,7 +3,7 @@ import React from 'react';
 import * as s from '../style.js';
 
 function Tagline({i, titles, captions, taglineStyle, titleStyle, captionStyle}) {
-  return ( titles.length || captions.lenght ?
+  return ( titles.length || captions.length ?
     <s.Tagline style={taglineStyle}>
       <s.Title style={titleStyle}>{titles[i]}</s.Title>
       <s.Caption style={captionStyle}>{captions[i]}</s.Caption>
