88294f4a7671c46d43a2dc842eeb53e4b8db76d0
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-06-14 14:40:17 +0200

    feat: spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.sass';
+
+
+export default () => (
+  <div className="spinner">
+    <div className="rect1"></div>
+    <div className="rect2"></div>
+    <div className="rect3"></div>
+    <div className="rect4"></div>
+    <div className="rect5"></div>
+  </div>
+);
