4e4173de1284218e82bacbfc5ee67dbdaa51f799
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-06-19 11:08:20 +0200

    refactor: navbar component split

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.sass';
+
+
+export default () => (
+  <div className="navbar">
+    <div className="title">GALLEREACT</div>
+    <div className="payoff">Most custom slider ever</div>
+  </div>
+);
