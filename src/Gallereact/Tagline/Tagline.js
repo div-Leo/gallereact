b4ba992ec4807de05c9d50c7f78b9330fb3842b4
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-06-22 01:44:00 +0200

    fix: show title caption

--- src/Gallereact/Tagline/Tagline.js
+++ src/Gallereact/Tagline/Tagline.js
@@ -3,7 +3,7 @@ import React from 'react';
 import * as s from '../style.js';
 
 function Tagline({curr, taglineStyle, titleStyle, captionStyle}) {
-  return ( curr.titles || curr.captions ?
+  return ( curr.title || curr.caption ?
     <s.Tagline style={taglineStyle}>
       <s.Title style={titleStyle}>{curr.titles}</s.Title>
       <s.Caption style={captionStyle}>{curr.captions}</s.Caption>
