a4ec53a6f1ccd911cdf73e2151c5c05c0486b540
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-07-04 18:16:46 +0200

    recator: hide usplash credentials

--- example/src/utils/unsplash.js
+++ example/src/utils/unsplash.js
@@ -1,13 +1,13 @@
 import Unsplash, { toJson } from 'unsplash-js';
 
 const unsplash = new Unsplash({
-  applicationId: '856a49ae384c50aca5ecfcd61037378e4fceaa65b1914cc924a4c6cd3faa2ee7',
-  secret: '9e81deea92517885ed3dfc73ef21c58c340ed9f444bf4a3d8c0c05141fa1f75b',
-  callbackUrl: 'urn:ietf:wg:oauth:2.0:oob'
+  applicationId: process.env.REACT_APP_APPLICATON_ID,
+  secret: process.env.REACT_APP_SECRET,
+  callbackUrl: process.env.REACT_APP_CALLBACK_URL
 });
 
 const unsplashPhotos = topic =>
-  unsplash.search.photos(topic, 1)
+  unsplash.search.photos(topic, 2)
   .then(toJson)
   .then(({results}) => {
     if (results.length) {
