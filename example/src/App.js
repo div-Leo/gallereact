2efa9a6533c0a1b24bee8773050bd8281f1c788d
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-02-10 13:12:09 +0100

    feat: description & Options

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Gallereact from 'gallereact-dev';
+import Gallereact from 'gallereact';
 
-import { filterImages, unsplashPhotos } from './utils';
+import { filterImages, unsplashPhotos, options } from './utils';
 import {
   CheckboxList,
   CodeBlock,
@@ -9,6 +9,8 @@ import {
   ColorPicker,
   Navbar,
   Spinner,
+  Options,
+  Footer,
 } from './components';
 
 import './App.sass';
@@ -20,7 +22,7 @@ const defaultCheckboxes = {
   transition: true,
   loop: true,
   autoPlay: false,
-  titles: true,
+  titles: false,
   captions: false,
   displayDot: true,
   displayArrows: true,
@@ -31,48 +33,29 @@ const defaultColors = {
   secondaryColor: '#333333',
 };
 
-const defaultStyle = {
-  containerStyle: {},
-  slideStyle: {
-    width: '80%',
-    height: '90%',
-    margin: '5% 10%',
-    boxShadow: '0 2px 20px -1px #2222',
-    position: 'relative',
-  },
-  taglineStyle: {
-    background: '#FFF',
-  },
-  titleStyle: {
-    fontWeight: 800,
-    fontSize: '1.2rem',
-  },
-  captionStyle: {},
-  dotStyle: {
-    width: '25px',
-    height: '3px',
-    borderRadius: 0,
-    margin: '0px',
-  },
-  arrowStyle: {},
-};
-
 const App = () => {
+  const [option, setOption] = useState(1);
+  const { defaultStyle, checkboxes } = options[option];
+
   const [images, setImages] = useState([]);
-  const [checkboxOpts, setCheckboxOpts] = useState(defaultCheckboxes);
+  const [checkboxOpts, setCheckboxOpts] = useState({ ...defaultCheckboxes, ...checkboxes });
   const [colors, setColors] = useState(defaultColors);
   const [style, setStyle] = useState(defaultStyle);
 
   useEffect(() => {
-    setCheckboxOpts(checkboxOpts => ({ ...checkboxOpts, swipe: true }));
-    unsplashPhotos('sea').then(images => images && setImages(images));
+    unsplashPhotos('italy').then(images => images && setImages(images));
   }, []);
+
+  useEffect(() => {
+    setCheckboxOpts({ ...defaultCheckboxes, ...checkboxes });
+    setStyle({ ...defaultStyle });
+  }, [option]);
 
   const { titles, captions } = checkboxOpts;
   return (
     <>
       <Navbar />
-      <div className="slider_container">
+      <div className="slider_container" style={style.mainStyle}>
         {images.length ? (
           <Gallereact
             images={filterImages(images, titles, captions)}
@@ -84,7 +67,19 @@ const App = () => {
           <Spinner />
         )}
       </div>
+      <div className="descrpition_container">
+        <h1>Gallereact</h1>
+        <p>
+          {' '}
+          Here you have a playground to try out all the possible customizations availables for
+          gallereact. <br /> Set the options you prefer then at the bottom of the page you can copy
+          your custom code snippet. <br /> For getting started check out the npm package{' '}
+          <a href="https://www.npmjs.com/package/gallereact">gallereact</a>
+        </p>
+      </div>
       <div className="settings_container">
+        <h1>Options</h1>
+        <Options {...{ setOption, option, options }} />
         <h1>Booleans</h1>
         <CheckboxList {...{ checkboxOpts, setCheckboxOpts }} />
         <h1>Colors</h1>
@@ -94,6 +89,7 @@ const App = () => {
         <h1>Output code</h1>
         <CodeBlock {...{ defaultCheckboxes, defaultColors, style, checkboxOpts, colors }} />
       </div>
+      <Footer />
     </>
   );
 };
